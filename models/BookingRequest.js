b54a6d5de2d89c0b3189e0f9aa712d2302a468b5
avinash kasukurthi <kasukurthiavinash16@gmail.com>
2025-12-09 19:59:25 +0530

    Add status field to BookingRequest schema

--- models/BookingRequest.js
+++ models/BookingRequest.js
@@ -1,23 +1,28 @@
-// backend/models/BookingRequest.js
 import mongoose from "mongoose";
 
 const BookingRequestSchema = new mongoose.Schema(
   {
-    id: { type: String, required: true, unique: true }, // req_...
+    id: { type: String, required: true, unique: true }, // "req_17336..."
     roomId: { type: String, required: true },
     bedId: { type: String, required: true },
 
     studentId: { type: String, required: true },
     studentName: { type: String, required: true },
 
+    status: {
+      type: String,
+      enum: ["Pending", "Approved", "Rejected"],
+      default: "Pending",
+    },
+
     timestamp: { type: Number, required: true },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
-export const BookingRequestModel = mongoose.model(
+const BookingRequest = mongoose.model(
   "BookingRequest",
   BookingRequestSchema
 );
+
+export default BookingRequest;
