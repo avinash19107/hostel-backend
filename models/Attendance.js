77559cc931b8f2ba25f8a6cc83df933fff37a373
avinash kasukurthi <kasukurthiavinash16@gmail.com>
2025-12-10 08:12:44 +0530

    Update Attendance.js

--- models/Attendance.js
+++ models/Attendance.js
@@ -3,26 +3,20 @@ import mongoose from "mongoose";
 
 const AttendanceRecordSchema = new mongoose.Schema(
   {
-    studentId: { type: String, required: true },
-    roomId: { type: String, required: true },
-    status: {
-      type: String,
-      enum: ["Present", "Absent"],
-      required: true,
-    },
+    userId: { type: String, required: true },
+    status: { type: String, enum: ["Present", "Absent"], required: true },
   },
   { _id: false }
 );
 
 const AttendanceSchema = new mongoose.Schema(
   {
-    date: { type: String, required: true }, // "2025-12-08"
-    hostelId: { type: String, required: true },
+    date: { type: String, required: true },       // "2025-12-10"
+    hostelId: { type: String, required: true },   // "G1", "B2", etc.
     records: { type: [AttendanceRecordSchema], default: [] },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
 export const AttendanceModel = mongoose.model("Attendance", AttendanceSchema);
+export default AttendanceModel;
